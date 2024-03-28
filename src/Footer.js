42e54ab2273aa5c3aad1ae73733e179618f87c4c
a.pogorzelski <pogorzelski.adrian@gmail.com>
2024-03-28 13:57:08 +0100

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 m-0 bg-neutral mt-5">
+            <p className="text-neutral-content">This is a project for recruitment purposes. Created by Adrian Pogorzelski.</p>
+        </footer>
+    )
+}
+
+export default Footer;
+
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="navbar bg-neutral">
+            <h1 className="btn btn-ghost text-xl text-neutral-content">Stack Overflow tag viewer</h1>
+        </header>
+    )
+}
+
+export default Header
